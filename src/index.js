195b1b3168528d6309e62b850eb8ae3e0c6dbc2e
Ryan Manse <ryanmanse@gmail.com>
2019-02-16 00:04:34 -0600

    React router is working.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
+import Router from './router'
 
 const App = () => <div className='App'>hack</div>
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Router />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
